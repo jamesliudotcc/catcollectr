8c26c0f592c43abef300f4a202a3d75f7a3db4be
jamesliudotcc <james@jamesliu.cc>
2019-02-18 18:50:44 -0800

    Done.

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,7 @@
 """ Models for cat collectr
 """
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Cat(models.Model):
@@ -12,6 +13,9 @@ class Cat(models.Model):
     breed = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
     age = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes = models.IntegerField(default=0)
 
     def __str__(self):
         return self.name
+
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,10 @@
 """Views for the main_app """
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
 
-from .models import Cat
-from .forms import CatForm
+from .models import Cat, User
+from .forms import CatForm, LoginForm
 
 
 def index(request):
@@ -22,5 +23,50 @@ def post_cat(request):
     form = CatForm(request.POST)
     if form.is_valid:
         cat = form.save(commit=False)
+        cat.user = request.user
         cat.save()
     return HttpResponseRedirect("/")
+
+
+def profile(request, username):
+    user = User.objects.get(username=username)
+    cats = Cat.objects.filter(user=user)
+    return render(request, "profile.html", {"username": username, "cats": cats})
+
+
+def login_view(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            u = form.cleaned_data["username"]
+            p = form.cleaned_data["password"]
+            user = authenticate(username=u, password=p)
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponseRedirect("/")
+                else:
+                    print("The account has been disabled")
+            else:
+                print("The login credentials are incorrect")
+    else:
+        form = LoginForm()
+        return render(request, "login.html", {"form": form})
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect("/")
+
+
+def like_cat(request):
+    cat_id = request.GET.get("cat_id", None)
+
+    likes = 0
+    if cat_id:
+        cat = Cat.objects.get(id=int(cat_id))
+        if cat is not None:
+            likes = cat.likes + 1
+            cat.likes = likes
+            cat.save()
+    return HttpResponse(likes)
